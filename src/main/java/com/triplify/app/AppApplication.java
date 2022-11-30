bf17b5af46023132557bd21c7b255fcfd28c8199
Naqeebali Shamsi <nq957544@dal.ca>
2022-11-30 02:31:42 -0400

    Group model added

--- src/main/java/com/triplify/app/AppApplication.java
+++ src/main/java/com/triplify/app/AppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class 	AppApplication {
+public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
