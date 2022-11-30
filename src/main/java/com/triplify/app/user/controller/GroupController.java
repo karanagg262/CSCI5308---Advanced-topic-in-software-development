e38a9652cc8db30e8e8a39e6a1922bace424effe
Naqeebali Shamsi <nq957544@dal.ca>
2022-11-30 02:36:22 -0400

    Added Group controller

--- src/main/java/com/triplify/app/user/controller/GroupController.java
+++ src/main/java/com/triplify/app/user/controller/GroupController.java
@@ -1,5 +1,6 @@
 package com.triplify.app.user.controller;
 
+import com.triplify.app.user.model.Group;
 import com.triplify.app.user.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/users")
-public class UserController {
+@RequestMapping(path = "api/v1/groups") //TODO: change url to a specific group based on user selection
+public class GroupController {
     @GetMapping
-    public List<User> getUsers() {
-        return List.of(new User());
+    public List<Group> getGroups() {
+        return List.of(new Group());
     }
+
 }
