e5580542deb6e19466c90927e4b5eb5bea7e8c7c
Viral Siddhapura <vr607491@dal.ca>
2022-12-08 11:35:16 -0400

    Login and register front back end

--- src/main/java/com/triplify/app/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.triplify.app.repo;
-
-import com.triplify.app.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findbyFirstName(String first_name);
-}
