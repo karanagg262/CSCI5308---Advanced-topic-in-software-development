ae43122f286228dddb1657c5ad3b9c2f9720f82e
Naqeebali Naveed Shamsi <nq957544@dal.ca>
2022-12-08 11:36:18 -0400

    Revert "Merge branch 'LoginAndRegisterFrontBackEnd' into 'develop'"
    This reverts merge request !9

---
+++ src/main/java/com/triplify/app/model/User.java
@@ -0,0 +1,127 @@
+package com.triplify.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+import java.time.Period;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    
+    @Column(unique = true)
+    @NotEmpty(message = "Username cannot be empty")
+    private String username;
+    @Column(unique = true)
+    @NotEmpty(message = "email cannot be empty")
+    private String email;
+    @NotEmpty(message = "First name cannot be empty")
+    private String first_name;
+    private String last_name;
+    @NotEmpty(message = "I think therefore, I am.")
+    private LocalDate dob;
+    @NotEmpty(message = "Remember what you were assigned at birth.")
+    private String gender;
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private String address;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", dob=" + dob +
+                ", gender='" + gender + '\'' +
+                ", age=" + calculateAge(dob) +
+                ", address='" + address + '\'' +
+                '}';
+    }
+    private int calculateAge(LocalDate dob){
+        LocalDate curDate = LocalDate.now();
+        if ((dob != null) && (curDate != null)){
+            return Period.between(dob, curDate).getYears();
+        }
+        else{
+            return 0;
+        }
+    }
+}
+
