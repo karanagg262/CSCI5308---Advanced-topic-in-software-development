3f083262221bacd84a65a25fb8cb7518babb02e5
JdJaku <jaimin.desai@onboardops.com>
2022-12-10 00:39:16 -0400

    Endpoint integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,45 @@
 import { useState } from 'react';
 import './App.css';
+import { URL } from './Constants';
 
 function App() {
 
   const [alreadyRegistered, updateAuthType] = useState(true);
+  const [firstname, updateFirstName] = useState("");
+  const [lastname, updateLastName] = useState("");
+  const [email, updateEmail] = useState("");
+  const [password, updatePassword] = useState("");
+
+  const onLogin = () => {
+    console.log("Login");
+    console.log(email);
+    console.log(password);
+
+    fetch(URL + 'users/login?emailAddress=' + email + '&password=' + password, {
+      method: 'POST',
+
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+  }
+
+  const onRegister = () => {
+    console.log("Register");
+    console.log(firstname);
+    console.log(lastname);
+    console.log(email);
+    console.log(password);
+
+    fetch(URL + 'users/register?firstname=' + firstname + '&lastname=' + lastname + '&emailAddress=' + email + '&password=' + password, {
+      method: 'POST',
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+  }
 
   return (
     <>
@@ -18,19 +54,21 @@ function App() {
             null
             :
             <>
-              <input className="first-name" placeholder='First Name' />
-              <input className="last-name"  placeholder='Last Name' />
+              <input className="first-name" placeholder='First Name' value={firstname} onChange={e => updateFirstName(e.target.value)} />
+              <input className="last-name" placeholder='Last Name' value={lastname} onChange={e => updateLastName(e.target.value)} />
             </>
           }
           <br />
-          <input className="email" placeholder='Email' />
+          <input className="email" placeholder='Email' value={email} onChange={e => updateEmail(e.target.value)} />
           <br />
-          <input className="password" placeholder='Password' />
+          <input className="password" placeholder='Password' value={password} onChange={e => updatePassword(e.target.value)} />
           <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '15px', marginBottom: '30px', marginInline: '30px', alignItems: 'center' }}>
             <div className={alreadyRegistered ? 'login-btn-selected' : 'login-btn'}
               onClick={() => {
                 if (!alreadyRegistered) {
                   updateAuthType(true);
+                } else {
+                  onLogin();
                 }
               }}
             >Login</div>
@@ -38,6 +76,8 @@ function App() {
               onClick={() => {
                 if (alreadyRegistered) {
                   updateAuthType(false);
+                } else {
+                  onRegister();
                 }
               }}
             >Register</div>
