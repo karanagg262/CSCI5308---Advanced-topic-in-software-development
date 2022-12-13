1908260626949581925c00c1ceec76dc6eb9cb17
Karan Aggarwal <karan@t86fe.wpa.dal.ca>
2022-12-13 18:06:57 -0400

    issue

--- src/main/java/com/triplify/app/expenseFeature/service/SendUserExpense.java
+++ src/main/java/com/triplify/app/expenseFeature/service/SendUserExpense.java
@@ -14,83 +14,84 @@ import static com.triplify.app.expenseFeature.database.ExpenseDatabaseContstant.
 import static com.triplify.app.expenseFeature.database.ExpenseDatabaseContstant.expenses_table_id_group_details;
 
 public class SendUserExpense implements ISendUserExpense {
+
     @Override
     public List<Expenses> fetchMyExpenses(Long userid) {
         List<Expenses> listOfuserExpenses = new ArrayList<>();
+
         try {
             Connection connection =
-                    DatabaseConnection.getInstance().getDatabaseConnection();
-            try {
+                DatabaseConnection.getInstance().getDatabaseConnection();
 
-                System.out.println(connection.getCatalog());
+            System.out.println(connection.getCatalog());
 
-                ResultSet userDetailsResultSet =
-                        connection.createStatement().executeQuery("select * from User_expenses");
+            ResultSet userDetailsResultSet =
+                    connection.createStatement().executeQuery("select * from User_expenses");
 
-                while (userDetailsResultSet.next()) {
-                    Long from_user_id = userDetailsResultSet.getLong("" + expenses_table_from_user_id);
-                    Long to_user_id = userDetailsResultSet.getLong("" + expenses_table_to_user_id);
-                    if ((from_user_id.equals(userid) || to_user_id.equals(userid)) && !from_user_id.equals(to_user_id)) {
+            while (userDetailsResultSet.next()) {
+                Long from_user_id = userDetailsResultSet.getLong("" + expenses_table_from_user_id);
+                Long to_user_id = userDetailsResultSet.getLong("" + expenses_table_to_user_id);
+                if((from_user_id.equals(userid) || to_user_id.equals(userid)) && !from_user_id.equals(to_user_id)) {
 
-                        Long id = userDetailsResultSet.getLong("" + expenses_table_id);
-                        String transaction_id = userDetailsResultSet.getString("" + expenses_table_transaction_id);
-                        String description = userDetailsResultSet.getString("" + expenses_table_description);
-                        Float amount = userDetailsResultSet.getFloat("" + expenses_table_amount);
-                        String currency = userDetailsResultSet.getString("" + expenses_table_currency);
-                        Long id_group_details = userDetailsResultSet.getLong("" + expenses_table_id_group_details);
+                    Long id = userDetailsResultSet.getLong("" + expenses_table_id);
+                    String transaction_id = userDetailsResultSet.getString("" + expenses_table_transaction_id);
+                    String description = userDetailsResultSet.getString("" + expenses_table_description);
+                    Float amount = userDetailsResultSet.getFloat("" + expenses_table_amount);
+                    String currency = userDetailsResultSet.getString("" + expenses_table_currency);
+                    Long id_group_details = userDetailsResultSet.getLong("" + expenses_table_id_group_details);
 
-                        Expenses expense = new Expenses();
-                        expense.setId(id);
-                        expense.setTransaction_id(transaction_id);
-                        expense.setDescription(description);
-                        expense.setAmount(amount);
-                        expense.setCurrency(currency);
-                        expense.setGroupid(id_group_details);
-                        expense.setFromuserid(from_user_id);
-                        expense.setTouserid(to_user_id);
-                        listOfuserExpenses.add(expense);
-                    }
+                    Expenses expense = new Expenses();
+                    expense.setId(id);
+                    expense.setTransaction_id(transaction_id);
+                    expense.setDescription(description);
+                    expense.setAmount(amount);
+                    expense.setCurrency(currency);
+                    expense.setGroupid(id_group_details);
+                    expense.setFromuserid(from_user_id);
+                    expense.setTouserid(to_user_id);
+                    listOfuserExpenses.add(expense);
                 }
-
-                if (connection != null) {
-                    connection.close();
-                }
-
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
             }
-        }catch (DatabaseExceptionHandler dbe) {
-            throw new RuntimeException(dbe);
+
+            if (connection != null) {
+                connection.close();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DatabaseExceptionHandler e) {
+            throw new RuntimeException(e);
         }
-            return listOfuserExpenses;
+        return listOfuserExpenses;
     }
     @Override
-    public float calculateTotalExpense(Long userid){
+    public float calculateTotalExpense(Long userid) {
         long total = 0;
         try {
             Connection connection =
-                    DatabaseConnection.getInstance().getDatabaseConnection();
-                ResultSet userDetailsResultSet =
+                DatabaseConnection.getInstance().getDatabaseConnection();
+            ResultSet userDetailsResultSet =
                     connection.createStatement().executeQuery("select * from User_expenses");
-                while (userDetailsResultSet.next()) {
-                    Long from_user_id = userDetailsResultSet.getLong("" + expenses_table_from_user_id);
-                    Long to_user_id = userDetailsResultSet.getLong("" + expenses_table_to_user_id);
-                    Float amount = userDetailsResultSet.getFloat("" + expenses_table_amount);
-                    if(from_user_id.equals(userid) && (to_user_id.equals(userid))){
-                        total = (long) (total + amount);
-                    }
-                    if(to_user_id.equals(userid) && !from_user_id.equals(to_user_id)) {
-                        total = (long) (total + amount);
-                    }
+            while (userDetailsResultSet.next()) {
+                Long from_user_id = userDetailsResultSet.getLong("" + expenses_table_from_user_id);
+                Long to_user_id = userDetailsResultSet.getLong("" + expenses_table_to_user_id);
+                Float amount = userDetailsResultSet.getFloat("" + expenses_table_amount);
+                if(from_user_id.equals(userid) && (to_user_id.equals(userid)) && from_user_id.equals(to_user_id)){
+                    total = (long) (total + amount);
                 }
-                if (connection != null) {
-                    connection.close();
+                if(to_user_id.equals(userid) && !from_user_id.equals(to_user_id)) {
+                    total = (long) (total + amount);
                 }
+            }
 
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            } catch (DatabaseExceptionHandler dbe) {
-            throw new RuntimeException(dbe);
+            if (connection != null) {
+                connection.close();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DatabaseExceptionHandler e) {
+            throw new RuntimeException(e);
         }
         return total;
     }
