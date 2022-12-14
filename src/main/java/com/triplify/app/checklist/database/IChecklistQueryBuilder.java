7170e618b69e664c8707aac59181a49e1f07f9e8
Karan Aggarwal <karan@t86fe.wpa.dal.ca>
2022-12-14 07:23:14 -0400

    TDD and refactoring and checklist

---
+++ src/main/java/com/triplify/app/checklist/database/IChecklistQueryBuilder.java
@@ -0,0 +1,9 @@
+package com.triplify.app.checklist.database;
+
+import com.triplify.app.checklist.model.Checklist;
+
+import java.sql.Connection;
+
+public interface IChecklistQueryBuilder {
+    public int insertChecklistQuery(final Checklist checklist, Connection connection);
+}
